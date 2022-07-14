cdddd0cc98ea10f46dcdd3e22b0b6adf54f3eaf7
迷子 (Maiko Tan) <maiko.tan.coding@gmail.com>
2022-07-14 07:31:40 +0000

    remove cli options

--- src/build.ts
+++ src/build.ts
@@ -1,17 +1,16 @@
 import { CAC } from 'cac'
 import esbuild from 'esbuild'
-import { config, esmConfig } from './esbuild-config'
+import { config, esmConfig } from './esbuild'
 
 export function apply(cac: CAC): CAC {
   cac
-    .command('build [config]')
-    .option('minify', 'minify the bundle', { default: true })
+    .command('build')
     .option('hybrid', 'build both esm and cjs', { default: false })
     .action(async (options) => {
-      esbuild.build(config)
+      await esbuild.build(config)
 
       if (options.hybrid) {
-        esbuild.build(esmConfig)
+        await esbuild.build(esmConfig)
       }
     })
 
