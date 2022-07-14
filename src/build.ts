983a93a29ea683142860baeb2460bfc9fc8bc1fa
迷子 (Maiko Tan) <maiko.tan.coding@gmail.com>
2022-07-14 04:00:27 +0000

    extract esbuild-config to its own file

--- src/build.ts
+++ src/build.ts
@@ -1,52 +1,17 @@
-import { readFile } from 'fs-extra'
-
 import { CAC } from 'cac'
-import esbuild, { BuildOptions } from 'esbuild'
-import { yamlPlugin } from 'esbuild-plugin-yaml'
+import esbuild from 'esbuild'
+import { config, esmConfig } from './esbuild-config'
 
 export function apply(cac: CAC): CAC {
   cac
     .command('build [config]')
     .option('minify', 'minify the bundle', { default: true })
     .option('hybrid', 'build both esm and cjs', { default: false })
-    .action(async (config, options) => {
-      const pkg = JSON.parse(await readFile('./package.json', 'utf-8'))
-      let entry = ['src/index.ts']
-      let output = 'dist/index.bundle.js'
-
-      if (config) {
-        const c = require(config)
-        entry = c.entry ?? entry
-        output = c.outfile ?? output
-      }
-      const buildOption: BuildOptions = {
-        bundle: true,
-        platform: 'node',
-        target: 'node12',
-        entryPoints: entry,
-        outfile: output,
-        external: [
-          ...Object.keys({
-            ...(pkg.dependencies ?? {}),
-            ...(pkg.devDependencies ?? {}),
-            ...(pkg.peerDependencies ?? {}),
-          }),
-        ],
-        minify: options.minify,
-        sourcemap: true,
-        plugins: [yamlPlugin({})],
-      }
-      esbuild.build({
-        ...buildOption,
-        format: 'cjs',
-      })
+    .action(async (options) => {
+      esbuild.build(config)
 
       if (options.hybrid) {
-        esbuild.build({
-          ...buildOption,
-          format: 'esm',
-          outfile: 'dist/index.esm.mjs',
-        })
+        esbuild.build(esmConfig)
       }
     })
 
