c669e4c0f656cb6fbccaa0f9d860763f62d37653
迷子 (Maiko Tan) <maiko.tan.coding@gmail.com>
2022-10-20 03:25:03 +0000

    style: fix format by prettier

--- src/build.ts
+++ src/build.ts
@@ -1,22 +1,15 @@
 import { build as esbuildBuild, BuildOptions } from 'esbuild'
 
-import {
-  config as defaultConfig,
-} from './esbuild'
+import { config as defaultConfig } from './esbuild'
 
 type Options = BuildOptions & { hybrid?: boolean }
 
-export default async function build(
-  config?: Options | Options[],
-  override?: Options,
-): Promise<void> {
+export default async function build(config?: Options | Options[], override?: Options): Promise<void> {
   if (!config || (Array.isArray(config) && config.length === 0)) {
     config = [defaultConfig]
   }
   if (!Array.isArray(config)) {
     config = [config]
   }
-  await Promise.all(
-    config.map((c) => esbuildBuild({ ...c, ...(override ?? {}) })),
-  )
+  await Promise.all(config.map((c) => esbuildBuild({ ...c, ...(override ?? {}) })))
 }
